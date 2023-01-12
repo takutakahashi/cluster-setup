989ac7c3b4993d479d490a188b4b3b7ec0b8a368
takutaka <takutaka@pepabo.com>
2023-01-13 01:33:14 +0900

    fix fsnotify and node name

--- assets/recipes/static_files.rb
+++ assets/recipes/static_files.rb
@@ -12,6 +12,14 @@ end
   end
 end
 
+execute "sudo sysctl fs.inotify.max_user_instances=1024" do
+  command "sudo sysctl fs.inotify.max_user_instances=1024"
+end
+
+execute "sudo sysctl fs.inotify.max_user_watches=524288" do
+  command "sudo sysctl fs.inotify.max_user_watches=524288"
+end
+
 #execute "reboot if needed" do
 #  command "ulimit -n |grep 65535 || reboot"
 #end
